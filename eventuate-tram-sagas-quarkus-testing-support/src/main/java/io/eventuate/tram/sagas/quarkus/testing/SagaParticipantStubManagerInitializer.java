c6ad0178575dd752b50a5886a7394cea32668f1b
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-02-12 19:46:00 +0300

    Supported in-memory integration tests for orders and customers.

--- eventuate-tram-sagas-quarkus-testing-support/src/main/java/io/eventuate/tram/sagas/quarkus/testing/SagaParticipantStubManagerInitializer.java
+++ eventuate-tram-sagas-quarkus-testing-support/src/main/java/io/eventuate/tram/sagas/quarkus/testing/SagaParticipantStubManagerInitializer.java
@@ -6,8 +6,10 @@ import io.quarkus.runtime.Startup;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 @Startup
+@Singleton
 public class SagaParticipantStubManagerInitializer {
 
   @Inject
---
+++ orders-and-customers-quarkus-in-memory-integration-tests/src/test/java/io/eventuate/examples/tram/sagas/ordersandcustomers/integrationtests/quarkus/OrdersAndCustomersInMemoryIntegrationTest.java
@@ -0,0 +1,8 @@
+package io.eventuate.examples.tram.sagas.ordersandcustomers.integrationtests.quarkus;
+
+import io.eventuate.examples.tram.sagas.ordersandcustomers.quarkus.tests.AbstractOrdersAndCustomersIntegrationTest;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class OrdersAndCustomersInMemoryIntegrationTest extends AbstractOrdersAndCustomersIntegrationTest {
+}
